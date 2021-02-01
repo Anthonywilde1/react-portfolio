0e8f1f5f5b4064fde8114ac9b642640069fdf6d6
Anthony Wilde <Anthonywilde117.com>
2021-02-01 15:05:04 +1100

    started work on React-app

---
+++ my-app/src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export class Nav extends Component {
+    render() {
+        return (
+            <div>
+                <nav>
+                    <a href="./Welcome.js">Welcome</a>
+                    <a href="./Projects.js">Projects</a>
+                    <a href="./Contactme.js">Contact Me</a>
+                    <a href="./Resume.js">Resume</a>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Nav
