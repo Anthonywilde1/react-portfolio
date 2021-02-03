b2cf88bc1a38c6b3a49639985d5d76cb96a0dc2d
Anthony Wilde <Anthonywilde117.com>
2021-02-03 14:58:24 +1100

    fixed download resume link and finished base css

--- portfolio/src/Resume.js
+++ portfolio/src/Resume.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import { 
+    Link
+} from "react-router-dom";
 
 export default class Resume extends Component {
     render() {
         return (
             <div class="resume">
                 <h2>If you would like to download my Resume please follow the link below</h2>
-                <a href="../docs/Anthony_Wilde_Resume.docx" download>My Resume</a> 
+                <Link to="/Anthony_Wilde_Resume.docx" target="_blank" download>My Resume</Link>                 
             </div>
         )
     }
